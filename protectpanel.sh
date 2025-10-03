e4035e488f6b3d71e7a7c2c3e086cce89d083bf7
lightsecret <ilhammusawa355@gmail.com>
2025-10-03 15:12:08 +0700

    Update protectpanel.sh

--- protectpanel.sh
+++ protectpanel.sh
@@ -111,7 +111,7 @@ elif [ "$OPSI" = "2" ]; then
     echo -e "${GREEN}✅ Restore & build selesai.${RESET}"
 
 elif [ "$OPSI" = "3" ]; then
-    bash <(curl -s https://raw.githubusercontent.com/syahrill11/protect.js/main/ireng.sh)
+    bash <(curl -s https://raw.githubusercontent.com/lightsecret/antirusuh/main/protectadmin.sh)
 
 else
     echo -e "${RED}❌ Opsi tidak valid.${RESET}"
